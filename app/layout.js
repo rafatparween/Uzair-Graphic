3103d7edd11acbd3b0e64c5669543b9c4e3f7d38
Rafat Parween <109865229+rafatparween@users.noreply.github.com>
2025-05-07 16:21:33 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata = {
-  title: "micro-sonic",
-  description: "micro-sonic",
+  title: "uzairgraphicsprinting",
+  description: "uzairgraphicsprinting",
 };
 
 export default function RootLayout({ children }) {
